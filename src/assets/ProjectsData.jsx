b9e9c44646b5669c3565095f164efb157f15d273
Shahid <132165646+Shahidsheikh786@users.noreply.github.com>
2024-01-31 20:12:27 +0530

    Update ProjectsData.jsx

--- src/assets/ProjectsData.jsx
+++ src/assets/ProjectsData.jsx
@@ -2,47 +2,72 @@
 
 import img_1 from "./about-image.png"
 import img_2 from "./Ecommerce-Project.jpeg"
-import img_3 from "./ecommerce.jpeg"
-import img_4 from "./fiver.png"
-import img_5 from "./blog-app.jpeg"
-
+import reactECommerce from "./ecommerce.jpeg"
+import fiverImg from "./fiver.png"
+import blogImg from "./blog-app.jpeg"
+import htmlEcommerceImg from "./ecommerce-2.jpeg"
+import quotesImg from "./quotes.jpeg"
+import todoListImg from "./todoList.jpeg"
+import watherImg from "./wather.jpeg"
+import imgSearchImg from ".img-search.jpeg"
 
 let ProjectsData = [
 
   {
     id: 1,
     name : "search-engine Website",
-    imageUrl : img_1,
+    imageUrl : imgSearchImg,
     diployedUrl : "https://shahidsheikh786.github.io/search-engine/",
    tag : ["All","Mejor"]
   },
   {
     id: 2,
     name : "Weather app Website",
-    imageUrl : img_2,
+    imageUrl : watherImg,
     diployedUrl : "https://shahidsheikh786.github.io/Weather-app/",
     tag : ["All","Minor"]
   },
   {
     id: 3,
     name : "E-Commerce app By React js",
-    imageUrl : img_3,
+    imageUrl : reactECommerce,
     diployedUrl : "https://react-ecommerce-nine-phi.vercel.app/",
     tag : ["All","Mejor"]
   },
   {
     id: 4,
     name : "Fiverr clone By React js",
-    imageUrl : img_4,
+    imageUrl : fiverImg,
     diployedUrl : "https://fiverr-clone-orpin.vercel.app/",
     tag : ["All","Mejor"]
   },
   {
     id: 5,
     name : "Blog app By React js",
-    imageUrl : img_5,
+    imageUrl : blogImg,
     diployedUrl : "https://blog-app-teal-one.vercel.app/",
     tag : ["All","Mejor"]
+  },
+  {
+    id: 6,
+    name : "Html Ecommerce website",
+    imageUrl : htmlEcommerceImg,
+    diployedUrl : "https://shahidsheikh786.github.io/Ecommerce-1/",
+    tag : ["All","Minor"]
+  },
+  {
+    id: 7,
+    name : "Quotes app with api",
+    imageUrl : quotesImg,
+    diployedUrl : "https://shahidsheikh786.github.io/Quotes-app/",
+    tag : ["All","Minor"]
+  },
+  {
+    id: 8,
+    name : "Todo list app with html js",
+    imageUrl : todoListImg,
+    diployedUrl : "https://shahidsheikh786.github.io/Todo-list-/",
+    tag : ["All","Minor"]
   },
   ]
   
